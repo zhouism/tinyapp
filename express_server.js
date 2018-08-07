c1ace4df431f4e70c45de69b5f9a57140944312b
Julia <juliazhouism@gmail.comgit config --global user.email juliazhouism@gmail.comgit confit --global user.name Julia>
2018-08-07 23:34:27 +0000

    urls show and index created

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,9 +17,24 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
 
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
 app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+    let templateVars = { greeting: 'Hello World!' };
+    res.render("hello_world", templateVars);
+  });
+
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+    let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+    res.render("urls_show", templateVars);
+  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
