fbf0dcbbd4c174cbe02cdf2c7829476c844de5d8
Julia <juliazhouism@gmail.comgit config --global user.email juliazhouism@gmail.comgit confit --global user.name Julia>
2018-08-08 01:21:28 +0000

    redirects short URL

--- express_server.js
+++ express_server.js
@@ -4,10 +4,16 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  
 };
+
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -31,6 +37,10 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
 app.get("/urls/:id", (req, res) => {
     let templateVars = { shortURL: req.params.id, urls: urlDatabase };
     res.render("urls_show", templateVars);
@@ -39,3 +49,23 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.post("/urls", (req, res) => {
+    let shortURL = generateRandomString(6);
+    urlDatabase[shortURL] = req.body.longURL;
+    res.redirect(`http://localhost:8080/urls/${shortURL}`)
+  });
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = 
+  res.redirect(longURL);
+});
+
+function generateRandomString(keyLength) {
+  var i, key = "", characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var charactersLength = characters.length;
+    for (i = 0; i < keyLength; i++) {
+        key += characters.substr(Math.floor((Math.random() * charactersLength) + 1), 1);
+    }
+    return key;
+}
