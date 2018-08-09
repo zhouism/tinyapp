2331e06317474d89b8cfe1084931f6a4ac0e2b09
Julia <juliazhouism@gmail.comgit config --global user.email juliazhouism@gmail.comgit confit --global user.name Julia>
2018-08-09 01:22:23 +0000

    new url updates database

--- express_server.js
+++ express_server.js
@@ -19,49 +19,47 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-
-// app.get("/hello", (req, res) => {
-//   res.end("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-app.get("/hello", (req, res) => {
-    let templateVars = { greeting: 'Hello World!' };
-    res.render("hello_world", templateVars);
-  });
+  res.json(urlDatabase);
+});
 
 app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
-    let templateVars = { shortURL: req.params.id, urls: urlDatabase };
-    res.render("urls_show", templateVars);
-  });
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase }; //only send the url neeed from the database
+  res.render("urls_show", templateVars);
+});
 
 app.post("/urls", (req, res) => {
-    let shortURL = generateRandomString(6);
-    urlDatabase[shortURL] = req.body.longURL;
-    res.redirect('/urls')
-  });
-
-app.get("/u/:shortURL", (req, res) => {
-    let shortURL = req.params.shortURL;
-    let longURL = urlDatabase[shortURL];
-    res.redirect(longURL);
-  });
+  let shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect('/urls')
+});
 
 app.post("/urls/:id/delete",(req, res) => {
-    let shortURL = req.params.id;
-    delete urlDatabase[shortURL];
-    res.redirect('/urls')
-  });
+  let shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  let newLongURL = req.body.new_url;
+  urlDatabase[shortURL] = newLongURL;
+  res.redirect("/urls");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
